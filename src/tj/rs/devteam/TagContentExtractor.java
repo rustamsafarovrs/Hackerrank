8615a7d5d5e5c423a5173966b5ea38a3914b8b8e
rustamsafarovrs <rs.programmer@hotmail.com>
2020-10-23 14:24:51 +0500

    added solutions

---
+++ src/tj/rs/devteam/TagContentExtractor.java
@@ -0,0 +1,26 @@
+package tj.rs.devteam;
+
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author Rustam Safarov(RS)
+ * created at 13.10.2020
+ */
+
+
+public class TagContentExtractor {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+
+            //Write your code here
+
+            testCases--;
+        }
+    }
+}
---
+++ src/tj/rs/devteam/TestClass.java
@@ -0,0 +1,19 @@
+package tj.rs.devteam;
+
+/**
+ * @author Rustam Safarov(RS)
+ * created at 20.10.2020
+ */
+
+class User {
+    String name;
+}
+
+public class TestClass {
+    User u = new User();
+
+    public TestClass(){
+        u.name = "salom";
+    }
+
+}
